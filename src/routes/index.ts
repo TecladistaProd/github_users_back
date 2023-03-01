83361338e95ae0bce379d90e14603b5734463179
Vitor Silva <vitor.silva@ubistart.com>
2023-03-01 19:15:34 -0300

    test(__tests__): tests configuration and test server

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,7 @@ import usersRoutes from './users';
 
 const routes = Router();
 
-routes.get('/', (_, res) =>
-  res.status(200).json({
-    name: 'SP Github API',
-  })
-);
+routes.get('/', (_, res) => res.status(200).send('SP Github API'));
 
 routes.use('/users', usersRoutes);
 
