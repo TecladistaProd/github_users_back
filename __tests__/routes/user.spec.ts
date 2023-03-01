25489a2da1c5a98cff065fac3a801bb6a983223c
Vitor Silva <vitor.silva@ubistart.com>
2023-03-01 19:38:37 -0300

    test(user.spec.ts): testing user details from /api/users/:username/details

--- __tests__/routes/user.spec.ts
+++ __tests__/routes/user.spec.ts
@@ -2,8 +2,8 @@ import { describe, it, expect } from 'vitest';
 import request from 'supertest';
 import App from '../../src/App';
 
-describe('Run server and test GET /users', () => {
-  it('Should get 30 users from /users', async () => {
+describe('Run server and test GET /api/users', () => {
+  it('Should get 30 users from /api/users', async () => {
     const data = await request(App).get('/api/users');
     const { users } = data.body as {
       users: Array<any>;
@@ -11,7 +11,7 @@ describe('Run server and test GET /users', () => {
     expect(data.statusCode).toBe(200);
     expect(users).toHaveLength(30);
   });
-  it('Should get nextLink and id from last user match in link from /users', async () => {
+  it('Should get nextLink and id from last user match in link from /api/users', async () => {
     const data = await request(App).get('/api/users');
     const { users, nextLink } = data.body as {
       users: Array<any>;
@@ -19,5 +19,17 @@ describe('Run server and test GET /users', () => {
     };
     expect(data.statusCode).toBe(200);
     expect(nextLink.match(users[users.length - 1].id)).toBeTruthy();
+  });
+});
+
+describe('Run server and test GET /api/users/:username/details', () => {
+  it('Should get valid id from /api/users/tecladistaprod/details', async () => {
+    const data = await request(App).get('/api/users/tecladistaprod/details');
+    const { id } = data.body as {
+      id: number;
+    };
+    expect(data.statusCode).toBe(200);
+    expect(id).toBeTypeOf('number');
+    expect(id).toBeTruthy();
   });
 });
