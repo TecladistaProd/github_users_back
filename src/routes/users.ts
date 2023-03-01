c568ef23aa167640718534787f5441b131c60acd
Vitor Silva <vitor.silva@ubistart.com>
2023-03-01 17:37:25 -0300

    feat(users): configure github service and get users endpoint

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,23 @@
+import GithubService from '@services/github';
 import { Router } from 'express';
 
 const userRoutes = Router();
 
-userRoutes.get('/', (_, res) =>
-  res.status(200).json({
-    name: 'Users',
-  })
-);
+userRoutes.get('/', async (req, res) => {
+  const { since } = req.query as { since?: string };
+  try {
+    const users = await GithubService.listUsers(since);
+    res.status(200).json({
+      users,
+      nextLink: `http://${req.hostname}${req.baseUrl}?since=${
+        users[users.length - 1]?.id
+      }`,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: 'An Error Occurred',
+    });
+  }
+});
 
 export default userRoutes;
--- src/services/github.ts
+++ src/services/github.ts
@@ -1,9 +1,17 @@
-import axios from 'axios';
+import github from 'octonode';
+import { IUser } from '@interfaces/user';
 
-const baseURL = process.env.GITHUB_URL;
+const client = github.client();
 
-const api = axios.create({
-  baseURL,
-});
+class GithubService {
+  static listUsers(since: string | number = 0): Promise<Array<IUser>> {
+    return new Promise((res, rej) => {
+      client.get('/users', { since, per_page: 10 }, (err, _, body) => {
+        if (err) rej(err);
+        res(body);
+      });
+    });
+  }
+}
 
-export default api;
+export default GithubService;
