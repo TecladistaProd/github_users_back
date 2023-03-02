7926034f20dc5e85d93a4a2d9bf64c815b471816
Vitor Silva <vitor.silva@ubistart.com>
2023-03-02 13:55:41 -0300

    fix: port

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import App from './App';
 import 'dotenv/config';
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3001;
 
 (async () => {
   App.listen(port, () => console.log(`Listening on port ${port}`));
